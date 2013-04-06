82dec354c0c847b0bd92484b5bcbc55ab9a621ff
Harry Metcalfe <harry@thedextrousweb.com>
2013-04-06 11:35:42 +0100

    Salary importer first stab

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,5 +1,5 @@
 class Salary < ActiveRecord::Base
   belongs_to :course
 
-  attr_accessible :agg, :instlq, :instmed, :instuq, :ldlq, :ldmed, :lduq, :lq, :med, :pop, :sbj, :uq
+  attr_accessible :agg, :instlq, :instmed, :instuq, :ldlq, :ldmed, :lduq, :lq, :med, :pop, :sbj, :uq, :id, :course_id
 end
