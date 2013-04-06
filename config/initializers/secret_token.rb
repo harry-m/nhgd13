cff62506fade12b3f062736d1b68152e697db981
Harry Metcalfe <harry@thedextrousweb.com>
2013-04-06 10:45:34 +0100

    Freshly brewed rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nhgd13::Application.config.secret_token = '7bc3ab02a9049697e6da9d5ed1b66221bc9ea1068ae78baa7677659663b528ab7c678fc3bc1eba149a3c8c4799a95bbe4e13b1227db8797a396e6d1307104ba1'
