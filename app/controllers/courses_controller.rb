26465f5049eb31fe6290933c1f90cb8318acc7a6
Harry Metcalfe <harry@thedextrousweb.com>
2013-04-06 16:46:54 +0100

    'fixed' that sorting error

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,7 +6,8 @@ class CoursesController < ApplicationController
 
   def results
     courses = Course.withsalary.where('title like ?', "%#{params[:terms]}%").limit(10)
-    @top_courses    = courses.order("instmed desc")
-    @bottom_courses = courses.order("instmed asc")
+
+    @top_courses    = courses.order("instmed desc").all.sort{|a,b| b.the_salary.instmed <=> a.the_salary.instmed}
+    @bottom_courses = courses.order("instmed asc").all.sort{|a,b| a.the_salary.instmed <=> b.the_salary.instmed}
   end
 end
